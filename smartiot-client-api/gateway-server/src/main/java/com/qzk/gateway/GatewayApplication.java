e966df1706592b42997323fe376d869b2b1dc910
qianzhikang <qzkqianzhikang@gmail.com>
2023-02-04 16:51:48 +0800

    微服务化

---
+++ smartiot-client-api/gateway-server/src/main/java/com/qzk/gateway/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.qzk.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description 网关服务
+ * @Date 2023-02-04-15-54
+ * @Author qianzhikang
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
